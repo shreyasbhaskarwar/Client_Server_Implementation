258635db197a47c5b61dc83f309674c16384e1fb
sbhaskarwar <hotshotthebest@gmail.com>
2023-10-03 18:01:45 -0400

    Use Async.StartChild instead of Async.Start

--- Server/Server.fs
+++ Server/Server.fs
@@ -13,17 +13,18 @@ let handleClient (clientSocket: TcpClient, clientId: int) =
         let reader = new StreamReader(stream)
         let writer = new StreamWriter(stream)
         
-        // Send the response back to the client
+        // Send the hello response to the client
         do! writer.WriteLineAsync("Hello!") |> Async.AwaitTask
         do! writer.FlushAsync() |> Async.AwaitTask
         
         let mutable response = Int32.MinValue
         
         while response <> -5 do
+            // Read the command send from client
             let! request = reader.ReadLineAsync() |> Async.AwaitTask
             Console.WriteLine($"Received: {request}")
 
-            // Process the request here and generate a response
+            // Process the command here and generate a result
             if request = "bye"
             then
                 response <- -5
@@ -43,13 +44,12 @@ let startServer () =
         let mutable clientId = 1
         
         listener.Start()
-        Console.WriteLine("Server started, waiting for connections...")
+        Console.WriteLine($"Server is running and listening on port {serverPort}.")
 
         while true do
             let! client = listener.AcceptTcpClientAsync() |> Async.AwaitTask
-            handleClient (client, clientId)
-            |> Async.Start
-            |> ignore
+            let! clientTask = handleClient (client, clientId) |> Async.StartChild
+            clientTask |> ignore
             clientId <- clientId + 1
     }
 
